850eccc0060ee383fb5515dfbc88dff9d7dafc1f
prathamesh22232 <146662828+prathamesh22232@users.noreply.github.com>
2025-04-16 17:53:17 +0530

    addition of node

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
    <>
     
       <Navbar />
-      <FirstYear/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
