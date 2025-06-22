c75985ba0a23da44c55f701801d4eaf3ca8085ca
Rupali <shirnathrupali@gmail.com>
2025-06-22 16:17:20 +0530

    added .env to fronend

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/createuser`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
